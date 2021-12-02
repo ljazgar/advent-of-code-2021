3a1ffaca63bc9504ea2df5dc063652b1fd7b5195
Łukasz Jazgar <lukasz.jazgar@miquido.com>
2021-12-02 08:52:16 +0100

    Day1, Day 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,42 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val numbers = input.map { it.toInt() }
+        var count = 0;
+        for (i in 1 until numbers.size) {
+            if (numbers[i] > numbers[i-1]) {
+                count++
+            }
+        }
+        return count;
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val numbers = input.map { it.toInt() }
+        val windowSums = ArrayList<Int>()
+        for (i in 0 until numbers.size - 2) {
+            windowSums.add(numbers.subList(i, i + 3).sum())
+        }
+
+        var count = 0;
+        for (i in 1 until windowSums.size) {
+            if (windowSums[i] > windowSums[i-1]) {
+                count++
+            }
+        }
+        return count;
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val part1TestResult = part1(testInput)
+    println("Part1: Test Result: " + part1TestResult)
+    check( part1TestResult == 7)
+
+    val part2TestResult = part2(testInput)
+    println("Part2: Test Result: " + part2TestResult)
+    check( part2TestResult == 5)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println("Part1: " + part1(input)) // 1393
+    println("Part2: " + part2(input)) // 1359
 }
---
+++ src/Day02.kt
@@ -0,0 +1,53 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        var x = 0
+        var depth = 0
+        input.forEach {
+            val splitted = it.split(' ')
+            val command = splitted[0]
+            val distance = splitted[1].toInt()
+
+            when (command) {
+                "forward" -> x += distance
+                "up" -> depth -= distance
+                "down" -> depth += distance
+            }
+        }
+        return x * depth
+    }
+
+    fun part2(input: List<String>): Int {
+        var x = 0
+        var depth = 0
+        var aim = 0
+        input.forEach {
+            val splitted = it.split(' ')
+            val command = splitted[0]
+            val distance = splitted[1].toInt()
+
+            when (command) {
+                "down" -> aim += distance
+                "up" -> aim -= distance
+                "forward" -> {
+                    x += distance
+                    depth += aim * distance
+                }
+            }
+        }
+        return x * depth
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day02_test")
+    val part1TestResult = part1(testInput)
+    println("Part1: Test Result: " + part1TestResult)
+    check( part1TestResult == 150)
+
+    val part2TestResult = part2(testInput)
+    println("Part2: Test Result: " + part2TestResult)
+    check( part2TestResult == 900)
+
+    val input = readInput("Day02")
+    println("Part1: " + part1(input))
+    println("Part2: " + part2(input))
+}
