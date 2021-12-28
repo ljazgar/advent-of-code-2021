f63786a7cdc63b935db02d6bdd081222bb601e72
Łukasz Jazgar <lukasz.jazgar@miquido.com>
2021-12-28 01:52:02 +0100

    Day 24 - approach 2

--- src/Day24.kt
+++ src/Day24.kt
@@ -1,103 +1,110 @@
 fun main() {
 
     // PART 1 ******************************************************************************
-    fun List<RPNElement>.getNumberIfNumberLiteral(): Int? {
-        val last = lastOrNull()
-        return if (last != null && last is NumberLiteral) last.value else null
+    data class VariableState(val possibleValues: Set<Long>)
+
+    fun calculateSet(s1: Set<Long>, s2: Set<Long>, operator: (Long, Long) -> Long): Set<Long> =
+        s1.asSequence().flatMap { i1 ->
+            s2.asSequence().map { i2 ->
+                operator(i1, i2)
+            }
+        }.toSet()
+
+    fun eval(p1: VariableState, p2: VariableState, operator: (Long, Long) -> Long): VariableState {
+        val possibleValues = calculateSet(p1.possibleValues, p2.possibleValues, operator)
+        return VariableState(possibleValues)
     }
 
-    fun transformOperation(operator: Operator, firstOperandFunction: List<RPNElement>, secondOperandFunction: List<RPNElement>): List<RPNElement> {
-        val firstOperandNumber = firstOperandFunction.getNumberIfNumberLiteral()
-        val secondOperandNumber = secondOperandFunction.getNumberIfNumberLiteral()
-        return when(operator.code) {
-            "add" -> {
-                when {
-                    firstOperandNumber == 0 -> secondOperandFunction
-                    secondOperandNumber == 0 -> firstOperandFunction
-                    firstOperandNumber != null && secondOperandNumber != null -> listOf(NumberLiteral(firstOperandNumber + secondOperandNumber))
-                    else -> listOf(firstOperandFunction, secondOperandFunction, listOf(operator)).flatten()
-                }
-            }
-            "mul" -> {
-                when {
-                    firstOperandNumber == 0 || secondOperandNumber == 0 -> listOf(NumberLiteral(0))
-                    firstOperandNumber == 1 -> secondOperandFunction
-                    secondOperandNumber == 1 -> firstOperandFunction
-                    firstOperandNumber != null && secondOperandNumber != null -> listOf(NumberLiteral(firstOperandNumber * secondOperandNumber))
-                    else -> listOf(firstOperandFunction, secondOperandFunction, listOf(operator)).flatten()
-                }
-            }
-            "div" -> {
-                when {
-                    firstOperandNumber == 0 -> listOf(NumberLiteral(0))
-                    secondOperandNumber == 0 -> throw error("Div by 0")
-                    secondOperandNumber == 1 -> firstOperandFunction
-                    firstOperandNumber != null && secondOperandNumber != null -> listOf(NumberLiteral(firstOperandNumber / secondOperandNumber))
-                    else -> listOf(firstOperandFunction, secondOperandFunction, listOf(operator)).flatten()
-                }
-            }
-            "mod" -> {
-                when {
-                    firstOperandNumber == 0 -> listOf(NumberLiteral(0))
-                    secondOperandNumber != null && secondOperandNumber <= 0 -> throw error("mod by 0 or negativ")
-                    secondOperandNumber == 1 -> listOf(NumberLiteral(0))
-                    firstOperandNumber != null && secondOperandNumber != null -> listOf(NumberLiteral(firstOperandNumber % secondOperandNumber))
-                    else -> listOf(firstOperandFunction, secondOperandFunction, listOf(operator)).flatten()
-                }
-            }
-            "eql" -> {
-                when {
-                    firstOperandFunction == secondOperandFunction -> listOf(NumberLiteral(1))
-                    firstOperandNumber != null && secondOperandNumber != null
-                        && firstOperandNumber != secondOperandNumber -> listOf(NumberLiteral(0))
-                    firstOperandFunction.last() is InputElement
-                        && secondOperandNumber != null &&(secondOperandNumber < 1 || secondOperandNumber > 9) -> listOf(NumberLiteral(0))
-                    secondOperandFunction.last() is InputElement
-                        && firstOperandNumber != null &&(firstOperandNumber < 1 || firstOperandNumber > 9) -> listOf(NumberLiteral(0))
-                    else -> listOf(firstOperandFunction, secondOperandFunction, listOf(operator)).flatten()
-                }
-            }
-            else -> throw error("unknown operator ${operator.code}")
+    fun add(p1: VariableState, p2: VariableState): VariableState = eval(p1, p2, Long::plus)
+    fun mul(p1: VariableState, p2: VariableState): VariableState = eval(p1, p2, Long::times)
+    fun div(p1: VariableState, p2: VariableState): VariableState = eval(p1, p2, Long::div)
+    fun mod(p1: VariableState, p2: VariableState): VariableState = eval(p1, p2, Long::mod)
+
+    fun eql(p1: VariableState, p2: VariableState): VariableState =
+        eval(p1, p2) { a, b ->
+            if (a == b) 1 else 0
+        }
+
+    fun inp(p1: VariableState, p2: VariableState): VariableState = p2
+
+    fun functionFor(instructionType: String): (VariableState, VariableState) -> VariableState {
+        return when(instructionType) {
+            "add" -> ::add
+            "mul" -> ::mul
+            "div" -> ::div
+            "mod" -> ::mod
+            "eql" -> ::eql
+            "inp" -> ::inp
+            else -> throw error("unknown instruction type $instructionType")
         }
     }
 
-    fun part1(instructions: Day24Input): Int {
-        val variablesFunctions = listOf('w', 'x', 'y', 'z')
-            .map { it to listOf<RPNElement>(NumberLiteral(0)) }
+    fun canGiveZZero(instructions: List<Instruction>, inputs: List<Int?>): Boolean {
+        val variablesState = listOf('w', 'x', 'y', 'z')
+            .map { it to VariableState(setOf(0)) }
             .toMap().toMutableMap()
 
         var inputIndex = 0
         for ((i, instruction) in instructions.withIndex()) {
-            val firstOperandFunction = variablesFunctions[instruction.variable] ?: throw error("Unknown variable: ${instruction.variable}")
-            val secondOperand = instruction.operand
-            val secondOperandFunction =
-                when (secondOperand) {
-                    is VariableOperand ->
-                        variablesFunctions[secondOperand.variable] ?: throw error("Unknown variable in operation operand: ${secondOperand.variable}")
-                    is NumberOperand -> listOf(NumberLiteral(secondOperand.value))
-                    else -> null
-                }
-            val newFunction =
-                when (instruction.type) {
-                    "inp" -> listOf(InputElement(inputIndex++))
-                    else -> transformOperation(Operator(instruction.type), firstOperandFunction, secondOperandFunction!!)
-                }
-            variablesFunctions[instruction.variable] = newFunction
-            println("After $i instruction: instruction length: w=${variablesFunctions['w']?.size} x=${variablesFunctions['x']?.size} y=${variablesFunctions['y']?.size} z=${variablesFunctions['z']?.size}")
-        }
+            val currVariableState = variablesState[instruction.variable] ?: throw error("Unknown variable: ${instruction.variable}")
 
-        val zFunction = variablesFunctions['z']!!
-        val res = zFunction.asSequence()
-            .mapNotNull { e -> if (e is InputElement) e else null }
-            .groupingBy { it.inputIndex }.eachCount()
-        println("Res: $res")
-        return 0
+            val operandState =
+                when (instruction.type) {
+                    "inp" ->
+                        inputs.getOrNull(inputIndex++)?.let { input -> VariableState(setOf(input.toLong())) }
+                            ?: VariableState((1L..9L).toSet())
+                    else -> {
+                        val operand = instruction.operand
+                            ?: throw error("Missing second operand for instruction ${instruction.type}")
+                        when (operand) {
+                            is VariableOperand -> variablesState[operand.variable]
+                                ?: throw error("No variable value: ${operand.variable}")
+                            is NumberOperand -> VariableState(setOf(operand.value.toLong()))
+                        }
+                    }
+                }
+
+            val resultVariableState = functionFor(instruction.type)(currVariableState, operandState)
+            variablesState[instruction.variable] = resultVariableState
+
+//            val variableStatesStr = variablesState.entries.joinToString(" ") { (v, state) ->
+//                "$v=(values:${state.possibleValues.size},min:${state.possibleValues.minOf { it }}, max:${state.possibleValues.maxOf { it }}, 0:${state.possibleValues.contains(0)},inputDeps:${state.dependsOnInput.size})" }
+//            println("After instruction $i: $variableStatesStr")
+        }
+        val zState = variablesState['z']!!
+        val zeroPossible = zState.possibleValues.contains(0L)
+        if (inputs.size <= 3) {
+            println("Input: $inputs  can return 0: $zeroPossible")
+        }
+        return zeroPossible
+    }
+
+    fun findRecursively(instructions: List<Instruction>, fixedModelNumberBeginning: List<Int>, dirDown: Boolean = true): List<Int>? {
+        if (fixedModelNumberBeginning.size == 14) {
+            return fixedModelNumberBeginning
+        }
+        val range = if (dirDown) 9 downTo 1 else 1..9
+        return range.asSequence()
+            .mapNotNull { i ->
+                val newFixedBeginning = fixedModelNumberBeginning + i
+                if (canGiveZZero(instructions, newFixedBeginning)) findRecursively(instructions, newFixedBeginning, dirDown)
+                else null
+            }
+            .firstOrNull()
+    }
+
+    fun part1(instructions: List<Instruction>): String? {
+        val resultList = findRecursively(instructions, emptyList())
+
+        return resultList?.joinToString("") { it.toString() }
     }
 
     // PART 2 ******************************************************************************
 
-    fun part2(input: Day24Input): Int {
-        return 0
+    fun part2(instructions: List<Instruction>): String? {
+        val resultList = findRecursively(instructions, emptyList(), false)
+
+        return resultList?.joinToString("") { it.toString() }
     }
 
     // ***********************************************************************************
@@ -108,44 +115,15 @@ fun main() {
     println("input: $input")
 
     // test if implementation meets criteria from the description:
-//    val part1TestResult = part1(testInput)
-//    println("Part1: Test Result: $part1TestResult")
-//    check( part1TestResult == 100000000)
     println("Part1: " + part1(input))
-
-//    val part2TestResult = part2(testInput)
-//    println("Part2: Test Result: $part2TestResult")
-//    check( part2TestResult == 100000000)
-//    println("Part2: " + part2(input))
+    println("Part2: " + part2(input))
 }
-
-//private sealed class Symbol
-//private class Variable(val name: Char) : Symbol() {
-//    companion object {
-//        val W = Variable('w')
-//        val X = Variable('x')
-//        val Y = Variable('y')
-//        val Z = Variable('z')
-//    }
-//}
-//private class Literal(val value: Int): Symbol()
-//
-//private sealed class Instruction(val type: String)
-//
-//private data class InpInstruction(val variable: Variable) : Instruction("inp")
-//private data class AddInstruction(val variable: Variable, val value: Symbol) : Instruction("add")
-//private data class MulInstruction(val variable: Variable, val value: Symbol) : Instruction("mul")
-//private data class DivInstruction(val variable: Variable, val value: Symbol) : Instruction("div")
-//private data class ModInstruction(val variable: Variable, val value: Symbol) : Instruction("mod")
-//private data class EqlInstruction(val variable: Variable, val value: Symbol) : Instruction("eql")
 
 private sealed class Operand
 private data class NumberOperand(val value: Int): Operand()
 private data class VariableOperand(val variable: Char): Operand()
 
 private data class Instruction(val type: String, val variable: Char, val operand: Operand?)
-
-private typealias Day24Input = List<Instruction>
 
 private fun String.parseOperand(): Operand =
     if (length == 1 && first().isLetter()) VariableOperand(first())
@@ -156,11 +134,6 @@ private fun parseInstruction(s: String): Instruction {
     return Instruction(words[0], words[1].first(), words.getOrNull(2)?.parseOperand())
 }
 
-private fun readDay24Input(name: String): Day24Input {
+private fun readDay24Input(name: String): List<Instruction> {
     return readInput(name).map { line -> parseInstruction(line) }.toList()
 }
-
-private sealed class RPNElement
-private data class Operator(val code: String): RPNElement()
-private data class NumberLiteral(val value: Int): RPNElement()
-private data class InputElement(val inputIndex: Int): RPNElement()
